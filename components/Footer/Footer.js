8dce1699a0bb0a6af1ef5c69ed2591ebe7e02301
Mario Hinostroza <mario.bba@hotmail.com>
2023-06-05 17:26:53 -0300

    update maro brip

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,5 +1,12 @@
 
 import './footer.css';
+import { FaPhoneAlt } from 'react-icons/fa';
+import { HiMail } from 'react-icons/hi';
+import { FaMapMarkerAlt } from 'react-icons/fa';
+import { AiFillInstagram } from 'react-icons/ai';
+
+import { AiFillFacebook } from 'react-icons/ai';
+
 import Image from 'next/image';
 
 export default function Footer() {
@@ -7,7 +14,7 @@ export default function Footer() {
         <>
             <footer className='container-footer'>
                 <div className='container-newsletter'>
-                    <p>Ofertas exclusivas por email</p>
+                    <p>Ofertas exclusivas de viajes por email</p>
                     <div className='child-container-newsletter'>
                         <input placeholder='Ingrese su email' />
                         <a className='btn-newsletter'>Subscribirme</a>
@@ -20,15 +27,15 @@ export default function Footer() {
                             <p>Tu agencia de viajes en todo el mundo.</p>
                         </div> */}
                         <div>
-                            <h4><strong>¿Necesitas ayuda?</strong></h4>
+                            <h4><FaPhoneAlt className='icon-tittle-footer' /><strong>¿Necesitas ayuda?</strong></h4>
                             <div className='flex-brip'>
-                                <a href="https://api.whatsapp.com/send/?phone=5114800534&amp;text&amp;type=phone_number&amp;app_absent=0">
+                                <a target="_blank" href="https://api.whatsapp.com/send/?phone=5114800534&amp;text&amp;type=phone_number&amp;app_absent=0">
                                     <img width={32} height={18} src="./icon-peru.png" />
                                     +51 14800534
                                 </a>
                             </div>
                             <div className='flex-brip'>
-                                <a href="https://api.whatsapp.com/send/?phone=5491152638334&amp;text&amp;type=phone_number&amp;app_absent=0">
+                                <a target="_blank" href="https://api.whatsapp.com/send/?phone=5491152638334&amp;text&amp;type=phone_number&amp;app_absent=0">
                                     <img width={32} height={34} src="./icon-arg.svg" />
                                     +54 1152638334
                                 </a>
@@ -36,24 +43,23 @@ export default function Footer() {
                         </div>
                         <div>
                             <h4><strong>Contactanos</strong></h4>
-                            <p><i class="fa-regular fa-envelope"></i>info@briptravel.com</p>
-                            <p>reservas@briptravel.com</p>
+                            <p><HiMail className='icon-footer-text'/>info@briptravel.com</p>
+                            <p><HiMail className='icon-footer-text'/>reservas@briptravel.com</p>
                         </div>
                         <div>
                             <h4><strong>Dirección</strong></h4>
-                            <p>122 y 53 bis</p>
-                            <p>44 y 56 Nº14</p>
-                            <p>2 y 56</p>
+                            <p><FaMapMarkerAlt className='icon-footer-location' />Av. Corrientes 3447</p>
+                            <p><FaMapMarkerAlt className='icon-footer-location' />Av. Corrientes 2451</p>
                         </div>
                         <div>
                             <h4><strong>Redes sociales</strong></h4>
-                            <p>Facebook Briptravel</p>
-                            <p>Instagram Briptravel</p>
+                            <a target="_blank" class="nombre-redes" href="https://www.facebook.com/bripargentina"><AiFillFacebook className='icon-footer-insta' />Facebook Briptravel</a>
+                            <a target="_blank" class="nombre-redes" href="https://www.instagram.com/bripargentina/"><AiFillInstagram className='icon-footer-insta' />Instagram Brip Travel</a>
                         </div>
                     </div>
-                    <div className="child-footer">
-                        <p>©2023 Briptravel. Todos los derechos reservados. </p>
-                    </div>
+                </div>
+                <div className="child-footer">
+                    <p>©2023 Briptravel. Todos los derechos reservados. </p>
                 </div>
             </footer>
         </>
